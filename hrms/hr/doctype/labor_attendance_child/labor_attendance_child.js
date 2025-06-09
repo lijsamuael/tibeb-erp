a2c954c718b4dac23505a4ac4ba04275aa871dfd
Tibeb ERP <lijsamuael>
2025-06-09 07:00:15 +0000

    jun 9 update

---
+++ hrms/hr/doctype/labor_attendance_child/labor_attendance_child.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Labor Attendance Child', {
+	// refresh: function(frm) {
+
+	// }
+});
