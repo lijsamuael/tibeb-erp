5d05721f8000a4fc590b20fb7d6c869c7cd6e477
Tibeb ERP <lijsamuael>
2025-06-09 07:01:00 +0000

    jun 9 update

---
+++ erpnext/support/doctype/fuel_disposal_table/fuel_disposal_table.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Fuel Disposal Table', {
+	// refresh: function(frm) {
+
+	// }
+});
